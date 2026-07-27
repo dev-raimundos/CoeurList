868f7e0aed3013b34c88e46e64fb2c207608a0b4
dev-raimundos <devraimundos@outlook.com>
2026-07-27 13:55:17 -0300

    Feat: adiciona biblioteca de componentes MudBlazer

--- CoeurList.Mobile/MauiProgram.cs
+++ CoeurList.Mobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace CoeurList
 {
@@ -15,6 +16,7 @@ namespace CoeurList
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
