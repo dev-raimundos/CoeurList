a4426155949243513de56f53efe4aed9c7087a09
dev-raimundos <devraimundos@outlook.com>
2026-07-27 17:32:36 -0300

    feat: adiciona APIs pra emitir output de mudança de tema.

--- CoeurList.Mobile/Platforms/Android/MainActivity.cs
+++ CoeurList.Mobile/Platforms/Android/MainActivity.cs
@@ -1,11 +1,50 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.View;
+using CoeurList.Services;
 
-namespace CoeurList
+namespace CoeurList.Platforms.Android
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        private ThemeService? _themeService;
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            _themeService = IPlatformApplication.Current?.Services.GetService<ThemeService>();
+            if (_themeService is not null)
+            {
+                _themeService.OnChange += UpdateStatusBarAppearance;
+                UpdateStatusBarAppearance();
+            }
+        }
+
+        private void UpdateStatusBarAppearance()
+        {
+            if (Window is null)
+            {
+                return;
+            }
+
+            var controller = WindowCompat.GetInsetsController(Window, Window.DecorView);
+            if (controller is not null)
+            {
+                controller.AppearanceLightStatusBars = !(_themeService?.IsDarkMode ?? false);
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            if (_themeService is not null)
+            {
+                _themeService.OnChange -= UpdateStatusBarAppearance;
+            }
+
+            base.OnDestroy();
+        }
     }
 }
--- CoeurList.Mobile/Platforms/Android/MainApplication.cs
+++ CoeurList.Mobile/Platforms/Android/MainApplication.cs
@@ -1,7 +1,7 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace CoeurList
+namespace CoeurList.Platforms.Android
 {
     [Application]
     public class MainApplication : MauiApplication
